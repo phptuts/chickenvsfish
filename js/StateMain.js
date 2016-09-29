0cfadab67cbc4696bf76040b1d1ebf9118805d23
phptuts <php.noah@gmail.com>
2016-09-28 23:20:38 -0500

    Got chicken to move on screen

--- js/StateMain.js
+++ js/StateMain.js
@@ -14,39 +14,41 @@ var StateMain={
         
         var background = game.add.image(0, 0, "background");
         chicken = game.add.sprite(100, 100, "chicken");
-        
         chicken.scale.setTo(.1, .1);
         chicken.anchor.setTo(.5, .5);
         game.physics.arcade.enable([chicken]);
         chicken.body.collideWorldBounds = true;
         chicken.body.gravity.y = 5000;
         chicken.body.bounce.y = .3;
+        chicken.body.bounce.x = .3;
+
         cursor = game.input.keyboard.createCursorKeys();
 
     },
     
     update:function()
     {       
+        this.controls();
+    },
+
+    controls: function() {
         if (cursor.up.isDown) {
             chicken.body.velocity.y = -400;
         }
-        else {
-            chicken.body.velocity.y = 0;
-        }
 
         if (cursor.down.isDown) {
-            chicken.body.velocity.x = 400;
-        }
-        else {
             chicken.body.velocity.x = 0;
+            chicken.body.velocity.y = 0;
+        }
+       
+        if (cursor.right.isDown) {
+            chicken.body.velocity.x = 400; 
+            chicken.scale.setTo(.1, .1);           
         }
 
         if (cursor.left.isDown) {
-            
-        }
-
-        if (cursor.right.isDown) {
-
+            chicken.body.velocity.x = -400;
+            chicken.scale.setTo(-.1, .1);
         }
     }    
     
