41bd95af3f542be2f40f6b801134fa03e3a8512e
phptuts <php.noah@gmail.com>
2016-09-29 22:26:36 -0500

    Got Chicken to collide with game world

--- js/StateMain.js
+++ js/StateMain.js
@@ -11,13 +11,19 @@ var StateMain={
     create:function()
     {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        
+        if (screen.width>1500)
+        {
+            game.world.setBounds(-20, 0, 660, 575);
+        }
+
         var background = game.add.image(0, 0, "background");
         chicken = game.add.sprite(100, 100, "chicken");
-        chicken.scale.setTo(.1, .1);
-        chicken.anchor.setTo(.5, .5);
+        chicken.scale.setTo(.2, .2);
+        chicken.anchor.setTo(0.5, 0.5);
+
         game.physics.arcade.enable([chicken]);
         chicken.body.collideWorldBounds = true;
+
         chicken.body.gravity.y = 5000;
         chicken.body.bounce.y = .3;
         chicken.body.bounce.x = .3;
@@ -43,12 +49,12 @@ var StateMain={
        
         if (cursor.right.isDown) {
             chicken.body.velocity.x = 400; 
-            chicken.scale.setTo(.1, .1);           
+            chicken.scale.setTo(.2, .2);           
         }
 
         if (cursor.left.isDown) {
             chicken.body.velocity.x = -400;
-            chicken.scale.setTo(-.1, .1);
+            chicken.scale.setTo(-.2, .2);
         }
     }    
     
