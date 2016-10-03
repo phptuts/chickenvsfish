c922725ce3def566cee2d44091c855d0db9ca831
phptuts <php.noah@gmail.com>
2016-10-02 23:25:30 -0500

    Getting Fish to animate on screen and create auto

--- js/StateMain.js
+++ js/StateMain.js
@@ -6,7 +6,7 @@ var StateMain={
     {
        game.load.image("background", "images/background.png");
        game.load.image("chicken", "images/chicken.png");
-       game.load.image("clownfish", "images/clownfish.png")
+       game.load.spritesheet("bluefish-h", "images/bluefish-h.png", 32, 20, 3);
     },
     
     create:function()
@@ -14,7 +14,7 @@ var StateMain={
         game.physics.startSystem(Phaser.Physics.ARCADE);
         if (screen.width>1500)
         {
-            game.world.setBounds(-20, 0, 660, 575);
+            game.world.setBounds(-20, 0, 680, 575);
         }
 
         var background = game.add.image(0, 0, "background");
@@ -24,7 +24,6 @@ var StateMain={
         chicken.anchor.setTo(0.5, 0.5);
 
         fishFoodGroup = game.add.group();
-        fishFoodGroup.scale.set(.05, .05);
         fishFoodGroup.setAll('anchor.x', 0.5);
         fishFoodGroup.setAll('anchor.y', 0.5);       
         fishFoodGroup.setAll('checkWorldBounds', true);   
@@ -36,7 +35,6 @@ var StateMain={
         chicken.body.gravity.y = 5000;
         chicken.body.bounce.y = .3;
         chicken.body.bounce.x = .3;
-        fishFoodGroup.setAll('body.velocity.x', 400);
         cursor = game.input.keyboard.createCursorKeys();
         
         game.time.events.loop(Phaser.Timer.SECOND *1, this.createFish, this);
@@ -48,11 +46,16 @@ var StateMain={
     },
 
     createFish: function() {
-        console.log(game.rnd.integerInRange(1000, 6000));
-        console.log(game.rnd.integerInRange(1000, 6000));
-        var fish =  fishFoodGroup.create(game.rnd.integerInRange(1000, 6000),game.rnd.integerInRange(1000, 6000), 'clownfish');
-        fish.body.collideWorldBounds = true;
-        fish.body.velocity.x = 4000;
+        var xPosition = 0;
+        var yPosition = game.rnd.integerInRange(100, 600);
+        var fish =  fishFoodGroup.create(xPosition, yPosition, 'bluefish-h');
+        fish.checkWorldBounds = true;
+        fish.body.outOfBoundsKill = true;
+        fish.body.enableBody = true;
+        fish.animations.add('swim');
+        fish.animations.play('swim', 6, true);
+        fish.body.velocity.x = 20;
+        fish.scale.x = -1;
 
     },
     
